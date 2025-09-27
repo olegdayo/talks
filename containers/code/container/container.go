6ca78157ea8654db0f5ed8bc6515647d6e61e966
olegdayo <ol-sidorenkov@mail.ru>
2025-09-27 14:34:07 +0300

    ls

--- containers/code/container/container.go
+++ containers/code/container/container.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"syscall"
 )
+
+type executable struct {
+	binary string
+	libs   []string
+}
 
 func setupUserspace(path string) error {
 	err := syscall.Mkdir(path, 0o777)
@@ -13,11 +20,49 @@ func setupUserspace(path string) error {
 		fmt.Println(err)
 	}
 
-	for _, sysDir := range []string{"/usr", "/lib", "/lib64"} {
-		cmd := exec.Command("cp", "-r", sysDir, path)
+	binPath := filepath.Join(path, "bin")
+	err = syscall.Mkdir(binPath, 0o777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	libPath := filepath.Join(path, "lib")
+	err = syscall.Mkdir(libPath, 0o777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	lib64Path := filepath.Join(path, "lib64")
+	err = syscall.Mkdir(lib64Path, 0o777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, executable := range []executable{
+		{
+			binary: "/usr/bin/sh",
+			libs:   []string{""},
+		},
+		{
+			binary: "/usr/bin/ls",
+			libs:   []string{"/usr/lib/libcap.so.2", "/usr/lib/libc.so.6", "/usr/lib64/ld-linux-x86-64.so.2"},
+		},
+	} {
+		cmd := exec.Command("cp", executable.binary, binPath)
 		err = cmd.Run()
 		if err != nil {
 			return err
+		}
+		for _, lib := range executable.libs {
+			targetLibPath := libPath
+			if strings.Contains(lib, lib64Path) {
+				targetLibPath = lib64Path
+			}
+			cmd := exec.Command("cp", lib, targetLibPath)
+			err = cmd.Run()
+			if err != nil {
+				return err
+			}
 		}
 	}
 
