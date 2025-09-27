250540c025b2ff348960756d750478b21a35ece4
olegdayo <ol-sidorenkov@mail.ru>
2025-09-27 09:24:27 +0300

    upds

--- containers/code/container/container.go
+++ containers/code/container/container.go
@@ -7,22 +7,33 @@ import (
 	"syscall"
 )
 
-func changeDirectory(path string) error {
+func setupUserspace(path string) error {
 	err := syscall.Mkdir(path, 0o777)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = syscall.Chdir(path)
+	for _, sysDir := range []string{"/usr", "/lib", "/lib64"} {
+		cmd := exec.Command("cp", "-r", sysDir, path)
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	err = os.Setenv("PATH", "/usr/bin")
 	if err != nil {
 		return err
 	}
 
-	err = syscall.Chroot(path)
+	return nil
+}
+
+func changeDirectory(path string) error {
+	err := syscall.Chroot(path)
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
@@ -63,6 +74,7 @@ func main() {
 	fmt.Println(argv)
 	switch argv[1] {
 	case "run":
+		setupUserspace("/tmp/container")
 		checkErr(changeDirectory("/tmp/container"))
 		checkErr(run(argv[2:]))
 	}
